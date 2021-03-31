7233bf87a0d3de99034db564711782f8f1166c06
robertagiannuzzi <80513519+robertagiannuzzi@users.noreply.github.com>
2021-03-31 12:23:16 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,7 +1,7 @@
 #My first code in R 
 
 #install.packages("raster") già fatto 
-library(raster) #
+library(raster) 
 #funzione brick è della libreria raster quindi non funzionerebbe senza il richiamo alla library raster
 
 setwd("C:/lab/")
@@ -24,3 +24,19 @@ plot(p224r63_2011, col=cl)
 #color change -> new
 cl <-colorRampPalette(c("green","magenta","orange","yellow")) (100)
 plot(p224r63_2011, col=cl)
+
+
+
+#31/03/2021
+#Multitemporal set
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_1988
+
+plot(p224r63_2011) #plottiamo le singole bande
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+
+#creare un multiframe con par e poi inserire le due immagini 1988 e 2011
+
+
+
