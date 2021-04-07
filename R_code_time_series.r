ed22ef8af751addbc7ee92c648d1cec0a93d3b43
robertagiannuzzi <80513519+robertagiannuzzi@users.noreply.github.com>
2021-04-07 10:39:24 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,9 @@
+#--------- 31/03/2021------------
+# Monitoraggio variazione delle temperature in Groenlandia
+#Data and code from Emanuela Cosma
+
+# install.packages("raster")
+
+setwd("C:/lab/Greenland") 
+
+ 
