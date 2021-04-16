f5435d0fa83dbcf98088835da7495b1bd9d393b6
robertagiannuzzi <80513519+robertagiannuzzi@users.noreply.github.com>
2021-04-16 09:32:14 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,9 @@
+# ---------16/04/2021--------
+
+# R_code_knitr.r -> COME CREARE UN REPORT CON R
+
+setwd("C:/lab/")
+
+#knitr prende un codice esterno e all'interno di r genera report salvato nella stessa cartella in cui ho il codice
+
+require(knitr) #require=library
