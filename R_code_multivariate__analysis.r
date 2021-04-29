5446f27b1db1b64e1abaa581dc45898be6dfc759
robertagiannuzzi <80513519+robertagiannuzzi@users.noreply.github.com>
2021-04-29 21:14:58 +0200

    Update R_code_multivariate__analysis.r

--- R_code_multivariate__analysis.r
+++ R_code_multivariate__analysis.r
@@ -13,8 +13,16 @@ p224r63_2011
 plot(p224r63_2011)
 
 plot(p224r63_2011$B1_sre, p224r63_2011$B2_sre, col="red", pch=19, cex=2)
+# 1° argomento = asse x
+# 2° argomento = asse y
 # col -> colora i punti
 # pch -> contrassegna i punti col modello 19 (da internte)
 # cex -> aumentare dimensione dei punti
 
+# Si ottiene un grafico rappresentante la riflettanza di ciascun pixel per le bande considerate
 
+# PAIRS -> plottare tutte le correlazioni possibili fra tutte le variabili ( cioè le bande) di un dataset
+pairs(p224r63_2011)
+# Indice di correlazione:  varia fra -1 e 1
+# -> se perfettamente correlati = 1
+# -> se mal correlati = -1 (all'aumentare di uno diminuisce l'altro)
