1ac070810347235d1289ef939ff73e5c2e874cd1
robertagiannuzzi <80513519+robertagiannuzzi@users.noreply.github.com>
2021-04-30 09:31:59 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -1,4 +1,4 @@
-# ----- 30/04/2021------
+# ----- 28/04/2021------
 
 library(raster)
 setwd("C:/lab/")
@@ -10,3 +10,16 @@ defor2 <- brick("defor2.jpg")
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+
+# ----- 30/04/2021--------
+
+defor1
+# nomi bande defor1.1; defor1.2; defor1.3
+dvi1 <- defor1$defor1.1 - defor1$defor1.2
+# differenza della riflettanza nell'infrarosso e nel rosso per ogni pixel di ciascuna banda
+
+#dev.off()
+plot(dvi1)
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plotRGB(dvi1, cl)
